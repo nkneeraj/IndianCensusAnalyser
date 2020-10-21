b304f672d6477bdb64d222307ca674f3ad1525db
nkneeraj <nkneerj641@gmail.com>
2020-10-21 16:13:29 +0530

    [NK]Added UC1:CensusState Analyser.

--- src/main/java/com/cg/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/cg/censusanalyser/StateCensusAnalyser.java
@@ -1,8 +1,41 @@
 package com.cg.censusanalyser;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class StateCensusAnalyser {
-	public static void main(String[] args) {
-		System.out.println("Welcome to census analyser");
+	public int loadStateCsvData(String csvFilePath) throws CensusAnalyserException {
+		if (!csvFilePath.contains(".csv")) {
+			throw new CensusAnalyserException(".csv file not found", CensusAnalyserException.ExceptionType.WRONG_TYPE);
+		}
+
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+			CsvToBeanBuilder<CsvStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			CsvToBean<CsvStateCensus> csvToBean = csvToBeanBuilder.withType(CsvStateCensus.class)
+					.withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<CsvStateCensus> csvItr = csvToBean.iterator();
+			Iterable<CsvStateCensus> csvIterable = () -> csvItr;
+			int noOfEntries = (int)StreamSupport.stream(csvIterable.spliterator(), false).count();
+			return noOfEntries;
+
+//			List<CsvStateCensus> list = new ArrayList<CsvStateCensus>();
+//			while(csvItr.hasNext())
+//				list.add(csvItr.next());
+//			return list.size();			
+		} catch (IOException e) {
+			throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		} catch (IllegalStateException e) {
+			throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		}
 	}
 
 }
