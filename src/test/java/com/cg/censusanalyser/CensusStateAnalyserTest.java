9ead39e137975a7950f20f7c7918a6f4902a193f
nkneeraj <nkneerj641@gmail.com>
2020-10-21 16:14:56 +0530

    mend
    [NK]Added UC1:CensusState Analyser.

--- src/test/java/com/cg/censusanalyser/CensusStateAnalyserTest.java
+++ src/test/java/com/cg/censusanalyser/CensusStateAnalyserTest.java
@@ -3,6 +3,7 @@ package com.cg.censusanalyser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusStateAnalyserTest {
 	
@@ -20,43 +21,5 @@ public class CensusStateAnalyserTest {
 		public void givenStateCensusCSVFile_ShouldReturnNumberOfRecords() throws CensusAnalyserException {
 			int noOfEntries = censusAnalyser.loadStateCsvData(STATE_CENSUS_FILE_PATH);
 			Assert.assertEquals(29, noOfEntries);
-		}
-	  
-	   
-//	   @Test
-//		public void givenStateCensus_WrongCSVFile_ShouldThrowException()  {
-//		   try {
-//			censusAnalyser.loadStateCsvData(STATE_CENSUS_WRONG_FILE_PATH);
-//		   }catch(CensusAnalyserException e) {
-//			   Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_CSV, e.type);
-//		   }
-//		}
-//	   
-//	   @Test
-//		public void givenStateCensus_WrongType_ShouldThrowException()  {
-//		   try {
-//			censusAnalyser.loadStateCsvData(STATE_CENSUS_WRONG_TYPE_FILE_PATH);
-//		   }catch(CensusAnalyserException e) {
-//			   Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_TYPE, e.type);
-//		   }
-//		}
-//	   
-//	   @Test
-//		public void givenStateCensus_WrongHeader_ShouldThrowException()  {
-//		   try {
-//			censusAnalyser.loadStateCsvData(STATE_CENSUS_WRONG_HEADER_FILE_PATH);
-//		   }catch(CensusAnalyserException e) {
-//			   Assert.assertEquals(CensusAnalyserException.ExceptionType.INTERNAL_ISSUE, e.type);
-//		   }
-//		}
-//	   
-//	   @Test
-//		public void givenStateCensus_WrongDelimiter_ShouldThrowException()  {
-//		   try {
-//			censusAnalyser.loadStateCsvData(STATE_CENSUS_WRONG_DELIMITER_FILE_PATH);
-//		   }catch(CensusAnalyserException e) {
-//			   Assert.assertEquals(CensusAnalyserException.ExceptionType.INTERNAL_ISSUE, e.type);
-//		   }
-//		}
-
+		}	  
 }
